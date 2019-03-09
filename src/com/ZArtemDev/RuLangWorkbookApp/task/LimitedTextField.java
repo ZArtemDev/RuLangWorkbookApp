61bd80b57f75d797f35384620fa19a9dca7a5cfd
ZArtemDev <ZArtemDev@gmail.com>
2019-03-10 00:50:38 +0300

    Commit 3: Task window without database

---
+++ src/com/ZArtemDev/RuLangWorkbookApp/task/LimitedTextField.java
@@ -0,0 +1,31 @@
+package com.ZArtemDev.RuLangWorkbookApp.task;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.control.TextField;
+
+public class LimitedTextField extends TextField {
+    LimitedTextField(){
+        super();
+        addTextLimiter(this, 1);
+        this.setPrefColumnCount(1);
+    }
+
+    LimitedTextField(int value){
+        super();
+        addTextLimiter(this, value);
+        this.setPrefColumnCount(value);
+    }
+
+    public static void addTextLimiter(final TextField tf, final int maxLength){
+        tf.textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                if(tf.getText().length() > maxLength){
+                    String s =tf.getText().substring(0, maxLength);
+                    tf.setText(s);
+                }
+            }
+        });
+    }
+}
---
+++ src/com/ZArtemDev/RuLangWorkbookApp/task/TaskController.java
@@ -0,0 +1,72 @@
+package com.ZArtemDev.RuLangWorkbookApp.task;
+
+import com.ZArtemDev.RuLangWorkbookApp.DBConnector;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class TaskController implements Initializable {
+
+    @FXML
+    TextFlow textFlow;
+
+    @FXML
+    Button btn;
+
+    private ObservableList list;
+
+    public void getResult(){
+        String rightResult = "e";
+        String s = "";
+        for(int i = 0; i < textFlow.getChildren().size(); i++){
+            if(textFlow.getChildren().get(i) instanceof TextField){
+                s += ((TextField) textFlow.getChildren().get(i)).getText();
+            }
+        }
+        if(s.equals(rightResult)){
+            System.out.println("That's right");
+        }else {
+            System.out.println("That's wrong");
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        String string = "           Дорогой _удрик!             \n " +
+                        "   Я х_чу рассказать тебе о своей ж_зни.\n" +
+                        "   Уч_ники старших кла__ов учстроили ля нас в школе живой уголок." +
+                        " _ам есть маленький ё_, черепаха, мышонок и черный у_." +
+                        " Все р_бята о них заботятся.\n" +
+                        "   Приезжай к нам!" ;
+
+        String[] strings = string.split("_");
+        list = textFlow.getChildren();
+
+        LimitedTextField textField = new LimitedTextField();
+        textField.setPrefColumnCount(1);
+
+        for(String s : strings ){
+            System.out.println(s);
+            if(!s.equals("")) {
+                list.add(new Text(s));
+                list.add(new LimitedTextField());
+            } else {
+                list.remove(list.size()-1);
+                list.add(new LimitedTextField(2));
+            }
+        }
+        list.remove(list.size()-1);
+        Button btn = new Button("Result");
+
+    }
+}
